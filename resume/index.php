07058e7d9944b13b90c662542c6ade5ea415e6a8
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-10-06 09:00:57 -0400

    feat: update resume

--- resume/index.php
+++ resume/index.php
@@ -1,3 +1,3 @@
 <?php
-header('Location: https://benhickson.com/resume/BenHicksonResume-2020-0625.pdf');
+header('Location: https://benhickson.com/resume/BenHicksonResume-SoftwareEngineer-2020-0916.pdf');
 exit;
